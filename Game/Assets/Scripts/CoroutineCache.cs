cda87e96fb683a45ab2235a2a4b59b78936b12fa
Gangbin12 <rudqls2023@naver.com>
2025-05-26 19:47:02 +0900

    5.26

---
+++ Game/Assets/Scripts/CoroutineCache.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineCache
+{
+    static Dictionary<float, WaitForSeconds> dictionary = new Dictionary<float, WaitForSeconds>();
+
+    public static WaitForSeconds WaitForSecond(float time)
+    {
+        WaitForSeconds waitForSeconds;
+
+        if(dictionary.TryGetValue(time, out waitForSeconds) == false)
+        {
+            dictionary.Add(time, new WaitForSeconds(time));
+
+            waitForSeconds = dictionary[time];
+        }
+
+        return waitForSeconds;
+    }
+
+}
