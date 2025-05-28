e2f1928c88cb5adff1819ba208cd6a3996ba4174
Gangbin12 <rudqls2023@naver.com>
2025-05-28 19:45:34 +0900

    5.28

--- Game/Assets/Scripts/SpeedManager.cs
+++ Game/Assets/Scripts/SpeedManager.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedManager : MonoBehaviour
+public class SpeedManager : Singleton<SpeedManager>
 {
     static SpeedManager instance;
 
     [SerializeField] float speed = 30.0f;
 
-    [SerializeField] float LimitSpeed = 60.0f;
+    [SerializeField] float limitSpeed = 60.0f;
 
     public float Speed { get { return speed; } }
 
     public static SpeedManager Instance { get { return instance; } }
 
-    private void Awake()
+    protected void Awake()
     {
+        base.Awake();
+
         if(instance == null)
         {
             instance = this; 
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Increase());
+    }
+
+    IEnumerator Increase()
+    {
+        while(speed < limitSpeed)
+        {
+            yield return CoroutineCache.WaitForSecond(5.0f);
+
+            speed = speed + 2.5f;
         }
     }
 
